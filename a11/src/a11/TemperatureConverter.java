e23c4c64d1d280ad970a1d2af3792962e25c5892
Rion Shekhovtsov <rion01045@gmail.com>
2019-11-20 19:35:37 +0300

    Add lesson6

---
+++ a11/src/a11/TemperatureConverter.java
@@ -0,0 +1,12 @@
+package a11;
+
+public class TemperatureConverter {
+	
+   public String convertTemp;
+   int temperature = 3;
+   char convertTo; 
+   public static void (String []Args);
+if (convertTo.equals "F" || convertTo.equals "C");
+{}
+}
+

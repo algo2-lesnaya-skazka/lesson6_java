2ee9e9fa8e190c92d2a3961f6d009d851fb0ae80
Chentsova Uliana <siora2203@gmail.com>
2019-11-26 18:49:50 +0300

    Add lesson6_java files

---
+++ car/src/lesson6/CarOwner.java
@@ -0,0 +1,12 @@
+package lesson6;
+
+public class CarOwner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		car myCarOwner = new car();
+		//myCarOwner.drive(0);
+		System.out.println(myCarOwner.drive(9));
+	}
+
+}
--- car/src/lesson6/car.java
+++ car/src/lesson6/car.java
@@ -1,10 +1,6 @@
 package lesson6;
 
 public class car {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//System.out.println("Hello World");
-	}	
 	//public void start()  
 	//public void stop()  
 	public int drive(int howlong) {

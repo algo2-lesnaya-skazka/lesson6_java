a88024f00dc09599d55aff1778e704eabc3d5de2
Michail Chernega <kakas.83@mail.ru>
2019-11-27 19:17:38 +0300

    Add lesson6\_java files

---
+++ Car/src/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+
+public static void main(String[] args) {
+
+}
+
+public void start() {}
+public void stop() {}
+public int drive(int howlong) {
+int distance = howlong*60;
+return distance;
+}
+}
---
+++ Car/src/CarOwner.java
@@ -0,0 +1,17 @@
+public class CarOwner {
+
+	public static void main(String... args) {
+
+
+
+		Car car =new Car();
+
+		car.start();
+
+		car.stop();
+
+		System.out.println(car.drive(2));
+
+
+	}
+}

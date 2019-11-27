e3c0706a2c3fa1a2916939aee967da6cdf204325
Stepanov Maxim <maksim_stepanov_06@mail.ru>
2019-11-27 19:11:20 +0300

    Add Car

---
+++ Car/src/Main/Car.java
@@ -0,0 +1,18 @@
+package Main;
+
+public class Car {
+
+	public static void main(String[] args) {
+		
+	}
+		
+		public void start() {}
+		public void stop() {}
+		public int drive(int howlong) {
+			int distance = howlong*60;
+			return distance;
+			}
+
+	
+
+}
---
+++ Car/src/Main/CarOwner.java
@@ -0,0 +1,14 @@
+package Main;
+
+public class CarOwner {
+	public static void main(String... args) {
+		
+	
+	Car car =new Car();
+	car.start();
+	car.stop();
+	System.out.println(car.drive(2));
+	
+}
+	
+}
